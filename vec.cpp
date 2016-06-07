e1c3c29e75b3bdecff4c8745837ec48fc90e2e08
nistor constantin <cnistor86@gmail.com>
2016-06-07 15:43:05 +0300

    basic functions
    added some basic funcions

--- vec.cpp
+++ vec.cpp
@@ -1,14 +1,81 @@
 #include "vec.h"
 
+#include<stdarg.h>
 
 
 namespace Adina {
-	vec::vec()
+	vec::vec():dim(0)
 	{
+		v = 0;
 	}
 
+	vec::vec(int dim):dim(dim)
+	{
+		v = new float[dim];
+		for (int i = 0; i < dim; i++) {
+			v[i] = i;
+		}
+	}
 
 	vec::~vec()
 	{
+		if (dim > 0) {
+			delete[] v;
+			dim = 0;
+		}
+	}
+	void vec::set(const float v[])
+	{
+		for (int i = 0; i < dim; i++) {
+			this->v[i] = v[i];
+		}
+	}
+	vec &vec::operator+(const vec & b)
+	{
+		vec* rez = new vec(dim);
+		for (int i = 0; i < dim; i++) {
+			rez->v[i] = v[i] + b.v[i];
+		}
+		return *rez;
+	}
+	vec &vec::operator-(const vec & b)
+	{
+		vec* rez = new vec(dim);
+		for (int i = 0; i < dim; i++) {
+			rez->v[i] = v[i] - b.v[i];
+		}
+		return *rez;
+	}
+	vec &vec::operator*(const vec & b)
+	{
+		vec* rez = new vec(dim);
+		for (int i = 0; i < dim; i++) {
+			rez->v[i] = v[i] * b.v[i];
+		}
+		return *rez;
+	}
+	vec &vec::operator*(const float & b)
+	{
+		vec* rez = new vec(dim);
+		for (int i = 0; i < dim; i++) {
+			rez->v[i] = this->v[i] * b;
+		}
+		return *rez;
+	}
+	bool vec::operator==(const vec & b)
+	{
+		for (int i = 0; i < dim; i++) {
+			if (v[i] != b.v[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+	std::ostream & operator<<(std::ostream & output, const vec & m)
+	{
+		for (int i = 0; i < m.dim; i++) {
+			output << m.v[i]<< std::endl;
+		}
+		return output;
 	}
 }
--- vec.h
+++ vec.h
@@ -1,10 +1,25 @@
 #pragma once
+
+#include<iostream>
+
 namespace Adina {
 
 	struct vec
 	{
+		float* v;
+		int dim = 0;
 		vec();
+		vec(int dim);
 		~vec();
+		///
+		void set(const float v[]);
+		/// operators
+		vec &operator+(const vec &b);
+		vec &operator-(const vec &b);
+		vec &operator*(const vec &b);
+		vec &operator*(const float &b);
+		bool operator==(const vec &b);
+		friend std::ostream &operator<<(std::ostream &output, const vec& m);
 	};
 
 }
