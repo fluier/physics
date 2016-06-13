9f8852fb746d3577efa2d59bfadf2c2663d89d8a
nistor constantin <cnistor86@gmail.com>
2016-06-13 21:57:55 +0300

    more on math
    implemented the operator*(const& vec, const& vec), this is giving a
    matrix

--- operators.cpp
+++ operators.cpp
@@ -1,5 +1,7 @@
 #include "operators.h"
 
+#include<iostream>
+
 Adina::vec & operator*(const Adina::mat & a, const Adina::vec & b)
 {
 	Adina::vec* rez = new Adina::vec(a.dim);
@@ -10,6 +12,21 @@ Adina::vec & operator*(const Adina::mat & a, const Adina::vec & b)
 		}
 		rez->v[i] = sum;
 		sum = 0;
+	}
+	return *rez;
+}
+
+Adina::mat& operator*(const Adina::vec & a, const Adina::vec & b)
+{
+	Adina::mat* rez = 0;
+	if (a.dim == b.dim) {
+		rez = new Adina::mat(a.dim);
+		for (int i = 0; i < rez->dim; i++) {
+			for (int j = 0; j < rez->dim; j++) {
+				rez->m[i][j] = a.v[i] * b.v[j];
+			}
+		}
+		return *rez;
 	}
 	return *rez;
 }
--- operators.h
+++ operators.h
@@ -4,3 +4,4 @@
 #include"mat.h"
 
 Adina::vec &operator*(const Adina::mat& a, const Adina::vec& b);
+Adina::mat &operator*(const Adina::vec& a, const Adina::vec& b);
